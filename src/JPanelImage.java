54c1b45e7a0ec2bb18fc3bd4ecdd33f04eecf0b0
Adrien <adrien.philippaux@gmail.com>
2016-03-04 11:41:16 +0100

    Enfin réussi pour l'image en background et le Jtextfield devant

--- src/JPanelImage.java
+++ src/JPanelImage.java
@@ -1,33 +1,25 @@
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class JPanelImage extends JPanel {
+	private Image img;
 
-  private Image backgroundImage;
-  private JTextField txt ;
-  public JPanelImage(String fileName) throws IOException {
-    backgroundImage = ImageIO.read(new File(fileName));
-    setLayout(new FlowLayout());
-    txt = new JTextField("Your Password");
-	txt.setVisible(true);
-	txt.setPreferredSize(new Dimension(200, 50));
-	
-	
-	this.add(txt);
-  }
+	public JPanelImage(Image img) {
+		this.img = img;
+		Color colBtn = new Color(206,251,247);
+		this.setLayout(null);
+		JTextField passwd = new JTextField();
+		
+		passwd.setBounds(120,200,200,30);
+		add(passwd);
+		
+	}
 
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-
-    // Draw the background image.
-    g.drawImage(backgroundImage, 0, 0, this);
-  }
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, null);
+	}
 }
--- src/Window.java
+++ src/Window.java
@@ -1,27 +1,39 @@
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
-public class Window {
+public class Window extends JPanel{
+	 private Image backgroundImage;
 
 	public Window() throws IOException {
+	    backgroundImage = ImageIO.read(new File("res/background.jpg"));
+
 		 JFrame f = new JFrame("Password Encryptor");
+		 f.getContentPane().setLayout(null);
+		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		 f.setPreferredSize(new Dimension(450, 650));
 		 f.setResizable(false);
 		 
-		
+		f.setContentPane(new JPanelImage(new ImageIcon("res/background.jpg").getImage()));		 
 		 
-		 
-		 f.getContentPane().add(new JPanelImage("res/background.jpg"));
 		 
 		 f.pack();
 		 f.setLocationRelativeTo(null);
 		 f.setVisible(true);
 	}
-	
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+
+	    // Draw the background image.
+	    g.drawImage(backgroundImage, 0, 0, this);
+	  }
 	public static void main(String[] args) throws IOException {
 		new Window();
 	}
